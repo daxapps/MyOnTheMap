a5c8378ca6a50f568f84cad05ccfc95de48a825c
daxapps <daxapps777@gmail.com>
2016-11-29 20:30:51 -0600

    MVP

--- MyOnTheMap/MapViewController.swift
+++ MyOnTheMap/MapViewController.swift
@@ -7,19 +7,174 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Check if current user has already posted a location
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ParseClient.sharedInstance().getUserLocation(userKey: UserInformation.userKey) { (success, error) in
+            
+            if success! {
+                
+                // If user is already on map, center map on coordinates they posted
+                
+                if UserInformation.latitude != 0.00 && UserInformation.longitude != 0.00 {
+                    
+                    performUIUpdatesOnMain {
+                        let location = CLLocationCoordinate2D(latitude: UserInformation.latitude, longitude: UserInformation.longitude)
+                        let span = MKCoordinateSpanMake(10, 10)
+                        let region = MKCoordinateRegion(center: location, span: span)
+                        self.mapView.setRegion(region, animated: true)
+                    }
+                    
+                }
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+            } else {
+                
+                self.displayAlert(message: error!)
+                
+            }
+            
+        }
+        
+        // Get student locations
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ParseClient.sharedInstance().getStudentLocations() { (success, error) in
+            
+            if success! {
+                
+                // Remove annotations and repopulate map
+                performUIUpdatesOnMain {
+                    self.mapView.removeAnnotations(self.mapView.annotations)
+                    self.populateMap()
+                }
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+            
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Actions
+    
+    @IBAction func logoutPressed(_ sender: AnyObject) {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        UdacityClient.sharedInstance().deleteSession() { (success, error) in
+            
+            if success {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                performUIUpdatesOnMain {
+                    let viewController = self.storyboard!.instantiateViewController(withIdentifier: "LoginViewController")
+                    self.present(viewController, animated: true, completion: nil)
+                }
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+        }
+        
     }
-
-
+    
+    @IBAction func refreshPressed(_ sender: AnyObject) {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ParseClient.sharedInstance().getStudentLocations() { (success, error) in
+            
+            if success! {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                // Remove map annotations and repopulate
+                performUIUpdatesOnMain {
+                    self.mapView.removeAnnotations(self.mapView.annotations)
+                    self.populateMap()
+                }
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+            
+        }
+        
+    }
+    
+    // MARK: Function to populate map with students
+    
+    func populateMap(){
+        
+        // Store map annotations
+        var annotations = [MKPointAnnotation]()
+        
+        for s in StudentModel.sharedInstance.students {
+            
+            // Create coordinate from latitude and longitude
+            let lat = CLLocationDegrees(s.latitude)
+            let lon = CLLocationDegrees(s.longitude)
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            
+            // Create map annotation with coordinate, name and media URL
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(s.firstName) \(s.lastName)"
+            annotation.subtitle = s.mediaURL
+            
+            // Add annotation to array
+            annotations.append(annotation)
+        }
+        
+        // Add annotations to map
+        mapView.addAnnotations(annotations)
+        
+    }
+    
+    // MARK: Open annotation's mediaURL in browser when tapped
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "MapViewAnnotation")
+        view.canShowCallout = true
+        view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+        return view
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        // Make sure URL is in correct format
+        let mediaURL = URL(string: Helpers.sharedInstance().formatURL(url: ((view.annotation?.subtitle)!)!))
+        UIApplication.shared.open(mediaURL!, options: [:], completionHandler: nil)
+    }
+    
 }
 
--- MyOnTheMap/TableViewController.swift
+++ MyOnTheMap/TableViewController.swift
@@ -8,18 +8,176 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // MARK: Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ParseClient.sharedInstance().getStudentLocations() { (success, error) in
+            
+            if success! {
+                
+                // Reload table data
+                performUIUpdatesOnMain {
+                    self.tableView.reloadData()
+                }
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Set number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StudentModel.sharedInstance.students.count
     }
-
-
+    
+    // Configure table cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        let studentTable = StudentModel.sharedInstance.students[indexPath.row]
+        
+        cell.textLabel?.text = studentTable.firstName + " " + studentTable.lastName
+        cell.detailTextLabel?.text = studentTable.mapString.capitalized
+        
+        return cell
+    }
+    
+    // Tap on table cell opens student's weblink
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Make sure URL is in correct format
+        if let url = URL(string: Helpers.sharedInstance().formatURL(url: StudentModel.sharedInstance.students[indexPath.row].mediaURL)) {
+            
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            
+        }
+        
+    }
+    
+    // MARK: Swipe to delete user information
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        // Check if row belongs to current user
+        // This is to make sure users can only delete their own information
+        
+        let thisRowStudent = StudentModel.sharedInstance.students[indexPath.row]
+        
+        return thisRowStudent.uniqueKey == UserInformation.userKey
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            // Identify student for this row
+            let thisRowStudent = StudentModel.sharedInstance.students[indexPath.row]
+            
+            // Remove the row
+            StudentModel.sharedInstance.students.remove(at: indexPath.row)
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            
+            // Remove user from Parse database
+            
+            ParseClient.sharedInstance().deleteUserLocation(objectId: thisRowStudent.objectId) { (success, error) in
+                
+                if success {
+                    
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                    
+                    // Reset user information locally
+                    // There's only one API call in map view to check whether current user has posted before
+                    // So this is to keep track of things internally
+                    
+                    UserInformation.latitude = 0.00
+                    UserInformation.longitude = 0.00
+                    
+                    UserInformation.mapString = ""
+                    UserInformation.mediaURL = ""
+                    UserInformation.objectId = ""
+                    
+                } else {
+                    
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                    self.displayAlert(message: error!)
+                    
+                }
+            }
+            
+            performUIUpdatesOnMain {
+                self.tableView.reloadData()
+            }
+            
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func logoutPressed(_ sender: AnyObject) {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        UdacityClient.sharedInstance().deleteSession() { (success, error) in
+            
+            if success {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                performUIUpdatesOnMain {
+                    let viewController = self.storyboard!.instantiateViewController(withIdentifier: "LoginViewController")
+                    self.present(viewController, animated: true, completion: nil)
+                }
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func refreshPressed(_ sender: AnyObject) {
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ParseClient.sharedInstance().getStudentLocations() { (success, error) in
+            
+            if success! {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                performUIUpdatesOnMain {
+                    self.tableView.reloadData()
+                }
+                
+            } else {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.displayAlert(message: error!)
+                
+            }
+            
+        }
+        
+    }
+    
 }
 
